efc2901dc4959c2badf0d92ff7f99c6087e8b777
Deepak Goswami <dipakgiree41@gmail.com>
2025-04-29 19:32:18 +0530

    Fix case-sensitivity issue in import path

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,12 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Login from './pages/Login';
 import ProjectCreationFlow from "./pages/ProjectCreationFlow";
-import UploadPage from "./pages/Uploadpage";
 import Editpage from "./pages/Editpage";
 import Account from "./pages/AccountSetting";
 import { useSelector } from "react-redux";
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import UploadPage from "./pages/Uploadpage";
 
 function App() {
   const islogin = useSelector((state) => state.userDetails.login);
